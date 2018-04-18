83dd628dfa08a3577aeb13f2500c075baeb8359a
Mitesh pc <mitesh.imeproit@gmail.com>
2018-04-18 12:24:09 +0530

    Initial Commit

---
+++ demoFacebookLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demoFacebookLogin
+//
+//  Created by Mitesh pc on 18/04/18.
+//  Copyright © 2018 Mitesh pc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
